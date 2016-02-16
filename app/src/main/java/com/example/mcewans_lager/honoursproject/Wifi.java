96fa1c546e23f5e41e34e54eaa077efcc0afa3ef
frasermcewan <qwb12173@uni.strath.ac.uk>
2016-02-16 12:54:08 +0000

    Added Intent Service for Wifi
    Small update to add the intent service

--- app/src/main/java/com/example/mcewans_lager/honoursproject/Wifi.java
+++ app/src/main/java/com/example/mcewans_lager/honoursproject/Wifi.java
@@ -26,6 +26,8 @@ public class Wifi extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wefee);
+        Intent intent = new Intent(this, WifiIntentService.class);
+        startService(intent);
 
 
 
