1def9c57200bb13c5487e665aa71723416d3d71b
frasermcewan <qwb12173@uni.strath.ac.uk>
2016-02-25 23:38:18 +0000

    Updating Service
    Small Change

--- app/src/main/java/com/example/mcewans_lager/honoursproject/WifiIntentService.java
+++ app/src/main/java/com/example/mcewans_lager/honoursproject/WifiIntentService.java
@@ -3,8 +3,14 @@ package com.example.mcewans_lager.honoursproject;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mcewans_lager on 16/02/16.
@@ -12,6 +18,12 @@ import android.util.Log;
 public class WifiIntentService extends IntentService {
 
     private static final String TAG = "Test";
+    TextView mainText;
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    StringBuilder sb = new StringBuilder();
+    static ArrayList<String> wList;
 
 
 
@@ -21,7 +33,22 @@ public class WifiIntentService extends IntentService {
     }
 
     protected void onHandleIntent(Intent intent) {
-        Log.i(TAG,"The service has begun");
+        WifiList wi = new WifiList(this);
+
+        wi.startWifi();
+        wi.getList();
+        wi.returnList();
+
+        Log.i(TAG, "The service has begun");
 
     }
+
+    public void startWifi () {
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+        mainText.setText("\nStarting Scan...\n");
+    }
+
 }
